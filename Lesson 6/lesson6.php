2dc04354c4b495239bb3ea2ec577ba3f3467a79d
Elton-777 <erositoni@yahoo.com>
2024-10-10 17:45:50 +0200

    Update lesson6.php

--- Lesson 6/lesson6.php
+++ Lesson 6/lesson6.php
@@ -1,5 +1,53 @@
 <?php 
 
-echo "HELLO"
+/*BREDHI LESSON 5*/
+
+echo "HAPPY NEW YEAR!";
+
+echo "<br>";
+echo "<br>";
+
+for($i=0; $i<5;$i++){
+    for ($k=15; $k>=$i ; $k--) { 
+        echo "&nbsp";
+    }
+    for($j=0; $j<=$i;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+for ($s=0; $s < 11; $s++) { 
+    echo "&nbsp";
+}
+echo "🎁!🎁";
+
+
+$employees = [
+    [
+        "name" => "Leart",
+        "age" => 15,
+        "position" => "CEO"
+    ],
+    [
+        "name" => "Dren",
+        "age" => 17,
+        "position" => "CEO2"
+    ],
+    [
+        "name" => "Blerta",
+        "age" => 24,
+        "position" => "CEO3"
+    ]
+    ];
+
+foreach ($employees as $ceos) {
+    echo "<ul>";
+    echo "<li>";
+    echo "Name:" .$ceos["name"];
+    echo "</li>";
+    echo "</ul>";
+}
+
+
 
 ?>
